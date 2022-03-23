81adf07c87d20f9c376fb7fc40c5c87dc134bb24
ed <cabrale234@gmail.com>
2022-03-22 23:27:23 -0400

    setting up cookies

--- src/components/AuthModal.js
+++ src/components/AuthModal.js
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
 const AuthModal = ({ setShowModal, isSignUp }) => {
   const [email, setEmail] = useState(null);
   const [password, setPassword] = useState(null);
   const [confirmPassword, setConfirmPassword] = useState(null);
   const [error, setError] = useState(null);
+  const [cookie, setCookie, removeCookie] = useCookies(["user"]);
 
-  let navigate = useNavigate;
+  let navigate = useNavigate();
 
   console.log(email, password, confirmPassword);
 
@@ -28,6 +30,10 @@ const AuthModal = ({ setShowModal, isSignUp }) => {
         email,
         password,
       });
+
+      setCookie("Email", response.data.email);
+      setCookie("UserId", response.data.userId);
+      setCookie("AuthToken", response.data.token);
 
       const success = response.status === 201;
       if (success) navigate("/onboarding");
