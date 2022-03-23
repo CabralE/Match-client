5ac4117db8308629872302aec4c1ad9f56d61e4f
ed <cabrale234@gmail.com>
2022-03-22 21:55:51 -0400

    more changes

--- src/components/AuthModal.js
+++ src/components/AuthModal.js
@@ -1,70 +1,83 @@
-import { useState } from "react"
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-const AuthModal = ({ setShowModal, isSignUp}) => {
-    const [email, setEmail] = useState(null)
-    const [password, setPassword] = useState(null)
-    const [confirmPassword, setConfirmPassword] = useState(null)
-    const  [error, setError] = useState(null)
+const AuthModal = ({ setShowModal, isSignUp }) => {
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [confirmPassword, setConfirmPassword] = useState(null);
+  const [error, setError] = useState(null);
 
-    console.log(email, password, confirmPassword)
+  let navigate = useNavigate;
 
-    const handleClick = () => {
-        setShowModal(false)
+  console.log(email, password, confirmPassword);
+
+  const handleClick = () => {
+    setShowModal(false);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (isSignUp && password !== confirmPassword) {
+        setError("Passwords need to match!");
+        return;
+      }
+      console.log("posting", email, password);
+      const response = await axios.post("http://localhost:8000/signup", {
+        email,
+        password,
+      });
+
+      const success = response.status === 201;
+      if (success) navigate("/onboarding");
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        try {
-            if(isSignUp && (password !== confirmPassword)){
-                setError("Passwords need to match!")
-            }
-            console.log('make a post request to our database')
-        } catch (error) {
-            console.log(error)
-        }
-    }
+  return (
+    <div className="auth-modal">
+      <div className="close-icon" onClick={handleClick}>
+        x
+      </div>
+      <h2>{isSignUp ? "CREATE ACCOUNT" : "Log In"}</h2>
+      <p>lorem20</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="email"
+          required={true}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="password"
+          required={true}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {isSignUp && (
+          <input
+            type="password"
+            id="password-check"
+            name="password-check"
+            placeholder="confirm password"
+            required={true}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        )}
+        <input className="secondary-button" type="submit" />
+        <p>{error}</p>
+      </form>
+      <hr />
+      <h2>GET THE APP</h2>
+      Auth Modal
+    </div>
+  );
+};
 
-    return(
-        <div className="auth-modal">
-            <div className="close-icon" onClick={handleClick}>x</div>
-            <h2>{isSignUp ? 'CREATE ACCOUNT' : 'Log In'}</h2>
-            <p>lorem20</p>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    placeholder="email"
-                    required={true}
-                    onChange={(e) => setEmail(e.target.value)}                
-                />
-                <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    placeholder="password"
-                    required={true}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                {isSignUp && <input
-                    type="password"
-                    id="password-check"
-                    name="password-check"
-                    placeholder="confirm password"
-                    required={true}
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                />}
-                <input 
-                className="secondary-button"
-                type="submit"
-                />
-                <p>{error}</p>
-            </form>
-            <hr/>
-            <h2>GET THE APP</h2>
-            Auth Modal
-        </div>
-    )
-}
-
-export default AuthModal
+export default AuthModal;
